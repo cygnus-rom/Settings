kardebayan/packages_apps_Settings

Settings: integrate spaceship into Settings
Change-Id: Ie98b73743b1907dd035030ce144be00b3759d1c3
Signed-off-by: Abhishek Singh <prince9singh@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -147,6 +147,8 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints.SavedAccessPointsWifiSettings;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
+import com.cygnus.spaceship.Spaceship;
+
 public class SettingsGateway {
 
     /**
@@ -281,7 +283,8 @@ public class SettingsGateway {
             PreviouslyConnectedDeviceDashboardFragment.class.getName(),
             BatterySaverScheduleSettings.class.getName(),
             MobileNetworkListFragment.class.getName(),
-            GlobalActionsPanelSettings.class.getName()
+            GlobalActionsPanelSettings.class.getName(),
+            Spaceship.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -320,5 +323,6 @@ public class SettingsGateway {
             Settings.MyDeviceInfoActivity.class.getName(),
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
+            Settings.SpaceshipActivity.class.getName(),
     };
 }

cygnus-rom/Settings

Signed-off-by: Dhruv <dhruvgera61@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -170,6 +170,8 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
+import com.cygnus.spaceship.Spaceship;
+
 public class SettingsGateway {
 
     /**
@@ -327,7 +329,8 @@ public class SettingsGateway {
             NetworkProviderSettings.class.getName(),
             NetworkSelectSettings.class.getName(),
             AlarmsAndRemindersDetails.class.getName(),
-            MediaManagementAppsDetails.class.getName()
+            MediaManagementAppsDetails.class.getName(),
+            Spaceship.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -368,5 +371,6 @@ public class SettingsGateway {
             Settings.MyDeviceInfoActivity.class.getName(),
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
+            Settings.SpaceshipActivity.class.getName(),
     };
 }